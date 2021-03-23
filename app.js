702ac251087f19733784044fffd74d6b5cdc10b6
AlexandraKrivonos <sashka.krivonos@gmail.com>
2021-03-24 00:04:15 +0300

    add: scripts for deploying

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const config = require('config')
+const path = require('path')
 const mongoose  = require('mongoose')
 
 const app = express()
@@ -8,6 +9,15 @@ app.use(express.json({extended : true}))
 
 app.use('/api/auth/', require('./routes/auth.routes'))
 app.use('/api/fanfic', require('./routes/fanfic.routes'))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+    app.get('*', (req,res) => {
+        res.sentFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+
+    })
+}
 
 
 const PORT = config.get('port') || 5000

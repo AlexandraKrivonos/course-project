d3702c7b8dc6933b15f29f3c434ca731e701ebad
AlexandraKrivonos <sashka.krivonos@gmail.com>
2021-03-28 21:22:24 +0300

    change app.get path

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use("/api/fanfic", require("./routes/fanfic.routes"));
 if (process.env.NODE_ENV === "production") {
   app.use("/", express.static(path.join(__dirname, "client", "build")));
 
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sentFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
